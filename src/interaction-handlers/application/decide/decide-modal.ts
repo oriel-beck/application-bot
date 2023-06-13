aa67f46635573c00513aae7ad6cfe3a3042a7e82
oriel-beck <orielbeck@gmail.com>
2023-06-13 12:51:16 +0300

    fix: this should be fine

--- src/interaction-handlers/application/decide/decide-modal.ts
+++ src/interaction-handlers/application/decide/decide-modal.ts
@@ -49,7 +49,7 @@ export class DecisionButtonHandler extends InteractionHandler {
     }
 
     async deny(interaction: ModalSubmitInteraction, application: types.Row, reason?: string) {
-        let res = await this.container.applications.removeTTL(application.get('user'), application.get('amswers'), application.get('questions'), application.get('message'), ApplicationState.denied).catch(() => null);
+        let res = await this.container.applications.update(application.user.toString(), 'state', ApplicationState.denied).catch(() => null);
 
         if (!res) {
             return interaction.reply({
@@ -69,7 +69,7 @@ export class DecisionButtonHandler extends InteractionHandler {
     }
 
     async accept(interaction: ModalSubmitInteraction, application: types.Row, reason: string) {
-        let res = await this.container.applications.removeTTL(application.get('user'), application.get('amswers'), application.get('questions'), application.get('message'), ApplicationState.accepted).catch(() => null);
+        let res = await this.container.applications.update(application.user.toString(), 'state', ApplicationState.accepted).catch(() => null);
 
         if (!res) {
             return interaction.reply({
