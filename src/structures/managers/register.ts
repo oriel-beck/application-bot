1ae95822650ee36f825f04b793ffab99972b10c3
oriel-beck <orielbeck@gmail.com>
2023-06-13 12:45:10 +0300

    fix: fix import

--- src/structures/managers/register.ts
+++ src/structures/managers/register.ts
@@ -1,7 +1,7 @@
 import './application.manager';
 import './blacklist.manager';
 import './question.manager';
-import './setting..manager';
+import './setting.manager';
 console.log('Assigning ApplicationManager to container.applications');
 console.log('Assigning BlacklistManager to container.blacklists');
 console.log('Assigning QuestionManager to container.questions');
