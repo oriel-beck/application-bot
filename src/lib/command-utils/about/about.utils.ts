177b52c495aac8bfe2c3c6862259abbc6ae3f6e9
oriel-beck <orielbeck@gmail.com>
2024-03-11 22:32:40 +0200

    modify about

--- src/lib/command-utils/about/about.utils.ts
+++ src/lib/command-utils/about/about.utils.ts
@@ -4,7 +4,7 @@ export function generateAboutEmbed() {
     return [
         new EmbedBuilder()
             .setTitle('About the bot')
-            .setDescription('**Framework**: [Sapphire](https://www.sapphirejs.dev/)\n**Created By**: <@311808747141857292>\n**Description**: This bot is an Application Bot created for BDFD, you can find the bot\'s source code on GitHub.\n\nThis bot is distributed under the MPL license.')
+            .setDescription('**Framework**: [Sapphire](https://www.sapphirejs.dev/)\n**Created By**: <@311808747141857292>\n**Description**: This bot is a Support Bot created for BDFD, you can find the bot\'s source code on [GitHub](https://github.com/oriel-beck/application-bot).\n\nThis bot is distributed under the MPL license.')
             .setColor(Colors.Blurple)
             .setFooter({ text: 'All rights reserved (C) - Oriel Beck' })
     ]
