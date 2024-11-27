177e8a9e3fb245ef90f2d0c0c5970d157c365e3c
oriel-beck <orielbeck@gmail.com>
2024-11-27 08:47:58 +0200

    insert the main guild config on startup

--- src/lib/db-register.ts
+++ src/lib/db-register.ts
@@ -1,6 +1,6 @@
 import { container } from "@sapphire/framework";
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
-import pkg from 'pg';                                                                                                                                     
+import pkg from 'pg';
 const { Pool } = pkg;
 
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
@@ -14,6 +14,9 @@ const db = drizzle(pool, { schema });
 
 console.log("migrating database");
 await migrate(db, { migrationsFolder: "/app/drizzle" })
+
+// try to insert the config for the main guild, if it fails then simply ignore it
+await db.insert(schema.settingsTable).values({ guild: BigInt(container.config.guild), enabled: false }).onConflictDoNothing();
 
 container.drizzle = db;
 console.log("registering database")
