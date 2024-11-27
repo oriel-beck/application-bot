9cedfccd940814d53e75f97df6f1fae3a1bbb495
oriel-beck <orielbeck@gmail.com>
2024-11-27 08:40:46 +0200

    recreate migrations

---
+++ drizzle/0000_unique_umar.sql
@@ -0,0 +1,37 @@
+CREATE TYPE "public"."state" AS ENUM('pending', 'denied', 'accepted', 'deleted', 'active');--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "applications" (
+	"user" bigint PRIMARY KEY NOT NULL,
+	"questions" json[] DEFAULT '{}' NOT NULL,
+	"answers" json[] DEFAULT '{}' NOT NULL,
+	"message" bigint,
+	"state" "state" NOT NULL,
+	"expiry" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "blacklist" (
+	"user" bigint PRIMARY KEY NOT NULL,
+	"reason" varchar(500),
+	"mod" bigint
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "messages" (
+	"id" bigint PRIMARY KEY NOT NULL,
+	"user" bigint NOT NULL,
+	"message" text NOT NULL,
+	"channel" bigint NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "questions" (
+	"id" uuid,
+	"question" varchar(500)
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "settings" (
+	"guild" bigint PRIMARY KEY NOT NULL,
+	"enabled" boolean
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "transcript" (
+	"author" bigint NOT NULL,
+	"channel" bigint PRIMARY KEY NOT NULL
+);
--- drizzle/0003_silly_makkari.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "messages" ALTER COLUMN "id" SET DATA TYPE bigint;
